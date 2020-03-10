aa5468f435cf8a9229eb146ebec4b861ea84e218
Rustam <q.rustam@code.edu.az>
2020-03-10 23:10:36 +0200

    Back http

--- constants/api.js
+++ constants/api.js
@@ -1,1 +1,1 @@
-export const base_url = "https://dataforest-dev.zg3jm3rumb.us-west-2.elasticbeanstalk.com/api";
+export const base_url = "http://dataforest-dev.zg3jm3rumb.us-west-2.elasticbeanstalk.com/api";
